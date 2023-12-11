fe96f976fe101e49100c68e14a2914c6f70aabe5
MikhailDeriabin <mikko.deriabin.work@gmail.com>
2023-12-11 16:47:43 +0200

    add BusinessProfileController

---
+++ API/Controllers/BusinessProfileController.cs
@@ -0,0 +1,42 @@
+using Application.BusinessProfileActions;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class BusinessProfileController : BaseApiController{
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] BusinessProfile body)
+    {
+        var result = await Mediator.Send(new Create.Command{BusinessProfile = body});
+        return HandleResponse(result);
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetMany()
+    {
+        var result = await Mediator.Send(new GetMany.Query());
+        return HandleResponse(result);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetActivityById([FromRoute] Guid id)
+    {
+        var result = await Mediator.Send(new GetOne.Query{ Id = id});
+        return HandleResponse(result);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateById([FromBody] BusinessProfile body)
+    {
+        var result = await Mediator.Send(new Update.Command{ BusinessProfile = body });
+        return HandleResponse(result);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteById([FromRoute] Guid id)
+    {
+        var result = await Mediator.Send(new Delete.Command{ Id = id});
+        return HandleResponse(result);
+    }
+}
