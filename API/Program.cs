e169112ac06e0ec51fa6e9c65defcf776369a491
MikhailDeriabin <mikko.deriabin.work@gmail.com>
2023-12-11 11:34:34 +0200

    update imports

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,10 @@
+using System;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
