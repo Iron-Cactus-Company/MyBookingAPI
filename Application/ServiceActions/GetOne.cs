fad01eb4fb6208a2cad7a308ffede79faf7f0867
MikhailDeriabin <mikko.deriabin.work@gmail.com>
2023-12-13 16:10:18 +0200

    add CRUD for Service entity

---
+++ Application/ServiceActions/GetOne.cs
@@ -0,0 +1,31 @@
+using Application.Core;
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.ServiceActions;
+
+public class GetOne
+{
+    public class Query : IRequest<Result<Service>>
+    {
+        public Guid Id{ get; init; }
+    }
+    
+    public class Handler : IRequestHandler<Query, Result<Service>>
+    {
+        private readonly DataContext _context;
+
+        public Handler(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Result<Service>> Handle(Query request, CancellationToken cancellationToken)
+        {
+           var result = await _context.Service.FindAsync(request.Id);
+           
+           return Result<Service>.Success(result);
+        }
+    }
+}
