cddcfbde9e6089789e736f4fd3cf5c9e0b173e59
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2023-12-13 12:40:02 +0200

    add Service entity

---
+++ Domain/Service.cs
@@ -0,0 +1,13 @@
+namespace Domain;
+
+public class Service
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string? Description { get; set; }
+    public int Duration { get; set; }
+    public int Price { get; set; }
+    
+    public Guid CompanyId { get; set; }
+    public Company Company { get; set; }
+}
