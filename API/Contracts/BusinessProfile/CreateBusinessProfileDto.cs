347fb168e1542b83f0a5887316ceeb1447ddbb1a
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-12-13 18:18:43 +0200

    add contracts

---
+++ API/Contracts/BusinessProfile/CreateBusinessProfileDto.cs
@@ -0,0 +1,15 @@
+﻿namespace API.Contracts.BusinessProfile
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class CreateBusinessProfileDto
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(30, MinimumLength = 6, ErrorMessage = "{0} must be between {2} and {1} character(s) in length.")]
+        public string Password { get; set; }
+    }
+}
---
+++ API/Contracts/BusinessProfile/UpdateBusinessProfileDto.cs
@@ -0,0 +1,19 @@
+﻿using API.Contracts.Shared;
+
+namespace API.Contracts.BusinessProfile
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class UpdateBusinessProfileDto
+    {
+        [Required(ErrorMessage = "Id is required")]
+        [GuidValidation]
+        public string Id { get; set; }
+        
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; }
+        
+        [StringLength(30, MinimumLength = 6, ErrorMessage = "{0} must be between {2} and {1} character(s) in length.")]
+        public string Password { get; set; }
+    }
+}
