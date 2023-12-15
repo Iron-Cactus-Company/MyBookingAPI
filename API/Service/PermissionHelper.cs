2a557d9c26afd76d1c8c6c8ec6c59ac8a0bb61c2
MikhailDeriabin <mikko.deriabin.work@gmail.com>
2023-12-15 14:26:51 +0200

    add overloadings for IsCompanyOwner

--- API/Service/PermissionHelper.cs
+++ API/Service/PermissionHelper.cs
@@ -20,4 +20,28 @@ public class PermissionHelper
 
         return company.Value.BusinessProfileId == businessProfileId;
     }
+    public async Task<bool> IsCompanyOwner(string businessProfileId, string companyId)
+    {
+        var company = await _mediator.Send(new GetOne.Query{Id = new Guid(companyId)});
+        if (company.Value == null)
+            return false;
+
+        return company.Value.BusinessProfileId.ToString() == businessProfileId;
+    }
+    public async Task<bool> IsCompanyOwner(Guid businessProfileId, string companyId)
+    {
+        var company = await _mediator.Send(new GetOne.Query{Id = new Guid(companyId)});
+        if (company.Value == null)
+            return false;
+
+        return company.Value.BusinessProfileId.ToString() == businessProfileId.ToString();
+    }
+    public async Task<bool> IsCompanyOwner(string businessProfileId, Guid companyId)
+    {
+        var company = await _mediator.Send(new GetOne.Query{Id = companyId});
+        if (company.Value == null)
+            return false;
+
+        return company.Value.BusinessProfileId.ToString() == businessProfileId;
+    }
 }
