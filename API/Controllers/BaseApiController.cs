ea1b1ba258f5bd8260ec69b2b26d7b2de5f34ad1
MikhailDeriabin <mikko.deriabin.work@gmail.com>
2023-12-17 18:01:59 +0200

    add methods for handling different basic responses

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,6 +1,9 @@
 using System.Security.Claims;
+using API.Contracts.BusinessProfile;
 using API.Enum;
 using Application.Core;
+using Application.Core.Error.Enums;
+using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,6 +15,8 @@ public class BaseApiController : ControllerBase{
     private IMediator _mediator;
     //Use the _mediator field, but if it is not defined use the one defined in the Program.cs
     protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+    
+    protected IMapper Mapper => HttpContext.RequestServices.GetService<IMapper>();
 
     protected string GetLoggedUserId()
     {
@@ -27,14 +32,53 @@ public class BaseApiController : ControllerBase{
         return (ProfileType)type;
     }
 
-    protected IActionResult HandleResponse<T>(Result<T> result)
+    protected IActionResult HandleCreateResponse<T>(Result<T> result)
     {
-        if (result == null)
-            return NotFound();
-        if (result.IsSuccess && result.Value != null)
-            return Ok(result);
-        if (result.IsSuccess && result.Value == null)
-            return NotFound();
+        if (result.IsSuccess)
+            return Created();
+        
+        if (result.Error.Type == ErrorType.NotFound)
+            return NotFound(result.Error);
+        if (result.Error.Type == ErrorType.NotUnique)
+            return Conflict(result.Error);
+
+        return BadRequest();
+    }
+    
+    protected IActionResult HandleReadResponse<TResult, TResponse>(Result<TResult> result)
+    {
+        if (result.IsSuccess)
+        {
+            var serializedResult = Mapper.Map<TResponse>(result.Value);
+            return Ok(serializedResult);
+        }
+        
+        if (result.Error.Type == ErrorType.NotFound)
+            return NotFound(result.Error);
+
+        return BadRequest();
+    }
+    
+    protected IActionResult HandleUpdateResponse<T>(Result<T> result)
+    {
+        if (result.IsSuccess)
+            return NoContent();
+        
+        if (result.Error.Type == ErrorType.NotFound)
+            return NotFound(result.Error);
+        if (result.Error.Type == ErrorType.NotUnique)
+            return Conflict(result.Error);
+
+        return BadRequest();
+    }
+    
+    protected IActionResult HandleDeleteResponse<T>(Result<T> result)
+    {
+        if (result.IsSuccess)
+            return NoContent();
+        
+        if (result.Error.Type == ErrorType.NotFound)
+            return NotFound(result.Error);
 
         return BadRequest();
     }
